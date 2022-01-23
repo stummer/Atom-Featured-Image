bb72bf448fef72ab81f6bd4c6a2c9c3b882ab718
stummer <henning.stummer@gmail.com>
2022-01-23 13:31:16 +0100

    Changed output from 'image' tag to 'icon' tag

--- inc/feed-image.php
+++ inc/feed-image.php
@@ -7,17 +7,17 @@ class FeedImage
     public static function register()
     {
         $image = new self();
-        add_action('atom_entry', array($image, 'create_atom_image_tag'));
+        add_action('atom_entry', array($image, 'create_atom_icon_tag'));
     }
 
 
-    public function create_atom_image_tag()
+    public function create_atom_icon_tag()
     {
         global $post;
         if (!has_post_thumbnail($post->ID))
             echo '';
 
         $options = Options::get_instance();
-        echo '<image>' . get_the_post_thumbnail_url($post->ID, $options->get('image_size'), ATOM_IMAGE_DEFAULT_SIZE) . '</image>';
+        echo '<icon>' . get_the_post_thumbnail_url($post->ID, $options->get('image_size'), ATOM_IMAGE_DEFAULT_SIZE) . '</icon>';
     }
 }
