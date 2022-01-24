b762c91a23e043cea2f9e5b8e66c431fd3c5439c
stummer <henning.stummer@gmail.com>
2022-01-24 23:33:10 +0100

    Added admin settings page functions

--- atom-featured-image.php
+++ atom-featured-image.php
@@ -1,23 +1,26 @@
 <?php
 /*
-Plugin Name: Atom Featured Image
-Plugin URI: https://github.com/stummer/Atom-Featured-Image
-Description: Adds the featured image of posts to the atom feed
-Version: 0.3.0
-Author: Henning Stummer
-Author URI: https://github.com/stummer
-License: GPLv3
+Plugin Name:       Atom Featured Image
+Plugin URI:        https://github.com/stummer/Atom-Featured-Image
+Description:       Adds the featured image to the atom feed post entry
+Version:           0.4.0
+Requires at least: 5.3
+Author:            Henning Stummer
+Author URI:        https://github.com/stummer
+License:           GPL v3 or later
+License URI:       https://www.gnu.org/licenses/gpl-3.0
+Text Domain:       my-basics-plugin
+Domain Path:       /languages
 */
 
 defined('ABSPATH') || exit();
 
-define('ATOM_IMAGE_CAPABILITY',   'customize'); // Admin
 define('ATOM_IMAGE_DEFAULT_SIZE', 'medium');
 
 
-require_once dirname(__FILE__) . '/inc/options.php';
-require_once dirname(__FILE__) . '/inc/feed-image.php';
-require_once dirname(__FILE__) . '/inc/admin-settings.php';
-require_once dirname(__FILE__) . '/inc/loader.php';
+require_once dirname(__FILE__) . '/includes/options.php';
+require_once dirname(__FILE__) . '/includes/feed-image.php';
+require_once dirname(__FILE__) . '/includes/admin-settings.php';
+require_once dirname(__FILE__) . '/includes/loader.php';
 
 \AtomFeaturedImage\Loader::init();
--- includes/admin-settings.php
+++ includes/admin-settings.php
@@ -25,7 +25,7 @@ class AdminSettings
 
         // General Section
         add_settings_section('atom_featured_image_general', __('General', 'atom_featured_image'), array($this, 'render_general_section'), 'atom_featured_image');
-        add_settings_field('atom_featured_image_size', __('Default Image Size', 'atom_featured_image'), array($this, 'render_size_field'), 'atom_featured_image', 'atom_featured_image_general');
+        add_settings_field('atom_featured_image_size', __('Feed Image Size', 'atom_featured_image'), array($this, 'render_size_field'), 'atom_featured_image', 'atom_featured_image_general');
     }
 
 
@@ -46,17 +46,20 @@ class AdminSettings
 
     public function render_general_section()
     {
-        ?>
-        <p><?php _e('Atom Featured Image Settings.', 'atom_featured_image'); ?></p>
-        <?php
+        echo '<p>' . _e('This Plugin adds an <icon> tag with the featured image to your atom feed.', 'atom_featured_image') . '</p>';
     }
 
 
     public function render_size_field()
     {
-      $options = Options::get_instance();
-        ?>
-        <input id="atom_featured_image_size" name="atom_featured_image[image_size]" type="number" value="<?php echo $options->get('image_size', ATOM_IMAGE_DEFAULT_SIZE); ?>" />
-        <?php
+        $sizes = wp_get_registered_image_subsizes();
+        $options = Options::get_instance();
+
+        echo '<select id="atom_featured_image_size" name="atom_featured_image[image_size]">';
+        foreach ($sizes as $name => $size)
+        {
+            printf('<option %s>%s</option>', $name == $options->get('image_size', ATOM_IMAGE_DEFAULT_SIZE) ? 'selected="selected"' : '', $name);
+        }
+        echo '</select>';
     }
 }
