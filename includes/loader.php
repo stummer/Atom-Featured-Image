c1d59d85ddc00180b3e27f8138d2d9eaad2cdd9b
stummer <henning.stummer@gmail.com>
2022-01-25 18:33:42 +0100

    Fixed display of translated texts

--- includes/loader.php
+++ includes/loader.php
@@ -6,6 +6,8 @@ class Loader
 {
     public static function init()
     {
+        load_plugin_textdomain('atom_featured_image', false, basename(ATOM_IMAGE_BASE_PATH) . '/languages' );
+
         $options = Options::get_instance();
 
         FeedImage::register();
