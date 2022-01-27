e046935a88850186897eb68f5cf73a4043844fcc
stummer <henning.stummer@gmail.com>
2022-01-27 19:29:02 +0100

    Add code comments

--- includes/loader.php
+++ includes/loader.php
@@ -1,9 +1,22 @@
 <?php
-
+/**
+* Atom Featured Image Plugin Loader
+*
+* @package Atom Featured Image
+* @since 1.0.0
+**/
 namespace AtomFeaturedImage;
 
+/*
+* Loader
+* @since 1.0.0
+*/
 class Loader
 {
+    /*
+    * Load text domain and options; register plugin classes
+    * @since 1.0.0
+    */
     public static function init()
     {
         load_plugin_textdomain('atom_featured_image', false, basename(ATOM_IMAGE_BASE_PATH) . '/languages' );
